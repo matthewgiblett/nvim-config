f479016941020bdc65a5f092a8cd61008c7c352f
Matthew Giblett <matthewgiblett@outlook.com>
2023-10-08 21:22:49 +1000

    tmux & lualine changes

--- lua/matthew/plugins/lualine.lua
+++ lua/matthew/plugins/lualine.lua
@@ -1,4 +1,12 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = true,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local lualine = require("lualine")
+        lualine.setup({
+            options = {
+                component_separators = { left = "", right = "" },
+            },
+        })
+    end,
 }
