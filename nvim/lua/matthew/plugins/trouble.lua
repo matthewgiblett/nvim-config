e67afb9ef38d3b9e7daf71c3afee0157851b7127
Matthew Giblett <matthewgiblett@outlook.com>
2024-06-19 21:46:42 +1000

    updated trouble.nvim config

--- nvim/lua/matthew/plugins/trouble.lua
+++ nvim/lua/matthew/plugins/trouble.lua
@@ -3,50 +3,34 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
 		{
-			"<leader>xd",
-			function()
-				require("trouble").toggle("document_diagnostics")
-			end,
-			desc = "Toggle document diagnostics list",
-		},
-		{
-			"<leader>xl",
-			function()
-				require("trouble").toggle("loclist")
-			end,
-			desc = "Toggle location list",
-		},
-		{
-			"<leader>xq",
-			function()
-				require("trouble").toggle("quickfix")
-			end,
-			desc = "Toggle quickfix list",
-		},
-		{
-			"<leader>xr",
-			function()
-				require("trouble").toggle("lsp_references")
-			end,
-			desc = "Toggle lsp reference list",
-		},
-		{
-			"<leader>xw",
-			function()
-				require("trouble").toggle("workspace_diagnostics")
-			end,
-			desc = "Toggle workspace diagnostics list",
-		},
-		{
 			"<leader>xx",
-			function()
-				require("trouble").toggle()
-			end,
-			desc = "Toggle trouble",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
 		},
-	},
-	opts = {
-		auto_preview = false,
-		use_diagnostic_signs = true,
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
 	},
 }
