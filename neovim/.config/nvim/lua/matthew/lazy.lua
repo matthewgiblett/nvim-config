a4821d709e16b1c555a908b603c108673473b125
Matthew Giblett <matthewgiblett@outlook.com>
2024-11-02 14:41:27 +1000

    feat: improve lazy.nvim installation setup

--- neovim/.config/nvim/lua/matthew/lazy.lua
+++ neovim/.config/nvim/lua/matthew/lazy.lua
@@ -1,17 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
+if not vim.uv.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		"--branch=stable",
+		lazyrepo,
 		lazypath,
 	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+		}, true, {})
+		return
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+local status_ok, lazy = pcall(require, "lazy")
+if not status_ok then
+	vim.api.nvim_echo({ "Installed lazy.nvim, please restart neovim" }, true, {})
+	return
+end
+
+lazy.setup({
 	{ import = "matthew.plugins" },
 	{ import = "matthew.plugins.lsp" },
 	{ import = "matthew.plugins.dap" },
