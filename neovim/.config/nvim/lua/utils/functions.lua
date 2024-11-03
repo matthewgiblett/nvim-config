34781dd46dd5653f5a817d349f74b1f1f1423049
Matthew Giblett <matthewgiblett@outlook.com>
2024-11-03 11:46:33 +1000

    feat(neovim): add file button function

--- neovim/.config/nvim/lua/utils/functions.lua
+++ neovim/.config/nvim/lua/utils/functions.lua
@@ -61,4 +61,41 @@ function F.button(shortcut, text, keybind, keybind_opts, highlight_opts)
 	}
 end
 
+--- @param filename string
+--- @param shortcut string
+--- @param short_filename string? optional
+--- @param highlight_opts table? optional settings: {text, file, shortcut}
+function F.file_button(filename, shortcut, short_filename, highlight_opts)
+	short_filename = short_filename or filename
+
+	-- Default highlight settings
+	highlight_opts = highlight_opts or {}
+	local hl_text = highlight_opts.text or "String"
+	local hl_file = highlight_opts.file or "Error"
+	local hl_shortcut = highlight_opts.shortcut or "Keyword"
+
+	local parent_directory = short_filename:match(".*[/\\]")
+	local icon, hl = F.get_icon(filename)
+	local text = icon .. "  " .. short_filename
+
+	local highlights = {}
+	table.insert(highlights, { hl_text, 0, #text })
+	if hl then
+		table.insert(highlights, { hl, 0, #icon })
+	end
+	if parent_directory ~= nil then
+		table.insert(highlights, { hl_file, #text - #short_filename + #parent_directory, #text })
+	end
+
+	local keybind_opts = {}
+	local button_hl_opts = {
+		text = highlights,
+		shortcut = hl_shortcut,
+	}
+	local button =
+		F.button(shortcut, text, "<cmd>e " .. vim.fn.fnameescape(filename) .. " <CR>", keybind_opts, button_hl_opts)
+
+	return button
+end
+
 return F
