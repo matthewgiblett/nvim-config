69fb51c55c4c9cc7c0ea2b4904ff213c997f2b66
Matthew Giblett <matthewgiblett@outlook.com>
2024-11-03 13:15:41 +1000

    feat(neovim): add function get_most_recent_files

--- neovim/.config/nvim/lua/utils/functions.lua
+++ neovim/.config/nvim/lua/utils/functions.lua
@@ -50,4 +50,44 @@ function F.echo_table_values(table)
 	end
 end
 
+--- @param start integer? optional default 1
+--- @param stop integer? optional default 9
+--- @param cwd string? optional current working directory
+--- @param opts table? optional
+function F.get_most_recent_files(start, stop, cwd, opts)
+	local default_mru_ignore = { "gitcommit" }
+	local default_opts = {
+		ignore = function(ignore_path, ext)
+			return (string.find(ignore_path, "COMMIT_EDITMSG")) or (vim.tbl_contains(default_mru_ignore, ext))
+		end,
+	}
+	local files = {}
+
+	start = start or 1
+	stop = stop or 9
+	opts = opts or default_opts
+
+	for _, v in pairs(vim.v.oldfiles) do
+		if #files == stop then
+			break
+		end
+
+		local is_cwd
+		if not cwd then
+			is_cwd = true
+		else
+			is_cwd = vim.startswith(v, cwd)
+		end
+
+		local is_readable = vim.fn.filereadable(v) == 1
+		local ignore = (opts.ignore and opts.ignore(v, F.get_extension(v))) or false
+
+		if is_cwd and is_readable and not ignore then
+			files[#files + 1] = v
+		end
+	end
+
+	return files
+end
+
 return F
