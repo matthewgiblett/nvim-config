f890a8d2b8911e06cea299561c44bbfd8d4d54c0
Matthew Giblett <matthewgiblett@outlook.com>
2024-11-07 12:32:46 +1000

    feat(neovim): adds nightfox highlight groups and modules

--- neovim/.config/nvim/lua/plugins/colourschemes/nightfox.lua
+++ neovim/.config/nvim/lua/plugins/colourschemes/nightfox.lua
@@ -7,12 +7,36 @@ return {
 			options = {
 				transparent = true,
 			},
+			modules = {
+				alpha = true,
+				cmp = true,
+				dap_ui = true,
+				fidget = true,
+				gitsigns = true,
+				illuminate = true,
+				indent_blankline = true,
+				lazy = true,
+				lsp_saga = true,
+				lsp_trouble = true,
+				nvimtree = true,
+				treesitter = true,
+				telescope = true,
+				whichkey = true,
+			},
+			groups = {
+				all = {
+					AlphaButton = { fg = "palette.blue.dim" },
+					AlphaFile = { fg = "palette.pink.bright" },
+					AlphaFooter = { fg = "palette.blue" },
+					AlphaHeader = { fg = "palette.blue.bright", style = "bold" },
+					AlphaHeading = { fg = "palette.cyan.bright", style = "bold" },
+					AlphaHeaderLabel = { fg = "palette.fg2", style = "italic" },
+					AlphaShortcut = { fg = "palette.blue.bright", style = "bold" },
+					NormalFloat = { bg = "none" },
+				},
+			},
 		})
 		-- variants: nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
 		vim.cmd([[colorscheme carbonfox]])
 	end,
-	AlphaButtons = "Boolean",
-	AlphaHeader = "Function",
-	AlphaHeaderLabel = "Comment",
-	AlphaShortcut = "Function",
 }
