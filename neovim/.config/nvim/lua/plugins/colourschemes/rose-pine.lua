ccc7b2af640df9481fb7ea45214705cec7381a33
Matthew Giblett <matthewgiblett@outlook.com>
2024-11-07 12:34:36 +1000

    feat(neovim): adds rose-pine transparency style and highlight groups

--- neovim/.config/nvim/lua/plugins/colourschemes/rose-pine.lua
+++ neovim/.config/nvim/lua/plugins/colourschemes/rose-pine.lua
@@ -6,13 +6,20 @@ return {
 		require("rose-pine").setup({
 			-- variants: main, moon, dawn
 			variant = "main",
-			disable_background = true,
+			styles = {
+				transparency = true,
+			},
+			highlight_groups = {
+				AlphaButton = { fg = "rose" },
+				AlphaFile = { fg = "love" },
+				AlphaFooter = { fg = "foam", italic = false },
+				AlphaHeader = { fg = "foam", bold = true },
+				AlphaHeading = { fg = "foam", bold = true },
+				AlphaHeaderLabel = { fg = "subtle", italic = true },
+				AlphaShortcut = { fg = "gold", bold = true },
+			},
 		})
 
 		vim.cmd([[colorscheme rose-pine]])
 	end,
-	AlphaButtons = "Tag",
-	AlphaHeader = "String",
-	AlphaHeaderLabel = "Operator",
-	AlphaShortcut = "Keyword",
 }
