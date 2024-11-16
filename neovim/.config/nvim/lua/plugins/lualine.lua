ce7725e8938e906f75ddc6bf7ed8f2efc6b356db
Matthew Giblett <matthewgiblett@outlook.com>
2024-11-16 18:41:19 +1000

    feat(neovim): change section components and separators

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -1,31 +1,72 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	event = "VimEnter",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "nvim-tree/nvim-web-devicons", "letieu/harpoon-lualine" },
 	config = function()
 		local lualine = require("lualine")
-		local lazy_status = require("lazy.status")
+		local utils = require("lualine.utils")
+
 		lualine.setup({
 			options = {
-				theme = vim.g.colourscheme,
-				component_separators = { left = "", right = "" },
+				theme = utils.transparent_auto(),
+				component_separators = {
+					left = "",
+					right = "",
+				},
+				section_separators = {
+					left = "", -- e0c6
+					right = "", -- e0c7
+				},
+				always_divide_middle = false,
+				disabled_filetypes = { "NvimTree" },
 			},
 			sections = {
+				lualine_a = {
+					"mode",
+				},
+				lualine_b = {
+					"file",
+					{
+						"diagnostics",
+						symbols = {
+							error = " ", -- f057
+							warn = " ", -- f071
+							info = " ", -- f05a
+							hint = " ", -- f0eb
+						},
+					},
+				},
 				lualine_c = {
-					function()
-						return require("auto-session.lib").current_session_name(true)
-					end,
+					"format",
+					"encodings",
+					"treesitter",
+					"venv",
+					"git",
 				},
 				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#c4a7e7" },
-					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
+					"mason",
+					"lazy",
 				},
+				lualine_y = {
+					"lsp_server",
+					"harpoon",
+				},
+				lualine_z = {
+					"position",
+				},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {
+					"file",
+				},
+				lualine_x = {
+					"harpoon",
+					"position",
+				},
+				lualine_y = {},
+				lualine_z = {},
 			},
 		})
 	end,
