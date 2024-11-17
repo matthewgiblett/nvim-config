4fd4b19f3fc38742739299e50474edf960971fb0
Matthew Giblett <matthewgiblett@outlook.com>
2024-11-17 11:25:02 +1000

    refactor(neovim): minor autopairs changes

--- neovim/.config/nvim/lua/plugins/autopairs.lua
+++ neovim/.config/nvim/lua/plugins/autopairs.lua
@@ -5,19 +5,10 @@ return {
 		"hrsh7th/nvim-cmp",
 	},
 	config = function()
-		local autopairs = require("nvim-autopairs")
+		require("nvim-autopairs").setup({})
+
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		local cmp = require("cmp")
-
-		autopairs.setup({
-			check_ts = true,
-			ts_config = {
-				lua = { "string" },
-				javascript = { "template_string" },
-				java = false,
-			},
-		})
-
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 	end,
 }
